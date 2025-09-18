517ad6dbb4cd37602bb242b0e735421a708758a8
mbasa <mario.basa@gmail.com>
2025-09-18 23:39:29 +0900

    changed st_overlap to st_within for mesh4 query.

--- src/main/java/org/mbasa/CensusService/repository/CustomRepository.java
+++ src/main/java/org/mbasa/CensusService/repository/CustomRepository.java
@@ -19,138 +19,138 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class CustomRepository {
 
-    /**
-     * コンストラクタ
-     *
-     */
-    public CustomRepository() {
+  /**
+   * コンストラクタ
+   *
+   */
+  public CustomRepository() {
+  }
+
+  @Autowired
+  private EntityManager em;
+
+  private String cleanJson(String geoJson) {
+    if (geoJson.startsWith("'") && geoJson.endsWith("'") &&
+        geoJson.length() >= 2) {
+      geoJson = geoJson.substring(1, geoJson.length() - 1);
+    }
+    if (geoJson.startsWith("\"") && geoJson.endsWith("\"") &&
+        geoJson.length() >= 2) {
+      geoJson = geoJson.substring(1, geoJson.length() - 1);
     }
 
-    @Autowired
-    private EntityManager em;
+    return geoJson;
+  }
 
-    private String cleanJson(String geoJson) {
-        if (geoJson.startsWith("'") && geoJson.endsWith("'") &&
-                geoJson.length() >= 2) {
-            geoJson = geoJson.substring(1, geoJson.length() - 1);
-        }
-        if (geoJson.startsWith("\"") && geoJson.endsWith("\"") &&
-                geoJson.length() >= 2) {
-            geoJson = geoJson.substring(1, geoJson.length() - 1);
-        }
+  public String queryMesh4(String geoJson) {
 
-        return geoJson;
-    }
+    geoJson = this.cleanJson(geoJson);
 
-    public String queryMesh4(String geoJson) {
+    String sql = String.format("""
+         WITH json_load AS (
+            SELECT ST_GeomFromGeoJSON('%s'\\:\\:JSONB->'geometry') json_geom
+        )
+        SELECT CAST(jsonb_build_object(
+            'type',     'FeatureCollection',
+            'features', jsonb_agg(feature)
+        ) AS TEXT)
+        FROM (
+          SELECT jsonb_build_object(
+            'type',       'Feature',
+            'id',       "MESH_CODE",
+            'geometry',   ST_AsGeoJSON(geom)\\:\\:JSONB,
+            'properties', to_jsonb(row) - "MESH_CODE" - 'geom'
+          ) AS feature
+          FROM (SELECT "MESH_CODE",
+            "人口総数",
+            "０〜１４歳人口総数",
+            "１５歳以上人口総数",
+            "１５〜６４歳人口総数",
+            "２０歳以上人口総数",
+            "６５歳以上人口総数",
+            "７５歳以上人口総数",
+            "外国人人口総数",
+            "世帯総数",
+            "一般世帯数",
+            "１人世帯の一般世帯数",
+            "２人世帯の一般世帯数",
+            "３人世帯の一般世帯数",
+            "４人世帯の一般世帯数",
+            "５人世帯の一般世帯数",
+            "６人世帯の一般世帯数",
+            "７人以上世帯の一般世帯数",
+            "６歳未満世帯員のいる一般世帯数",
+            "６５歳以上世帯員のいる一般世帯数",
+            "世帯主の年齢が２０〜２９歳の１人世帯の一般世帯数",
+            "高齢単身の一般世帯数",
+            "高齢夫婦のみの一般世帯数",
+            geom
+            FROM mesh4 m,json_load j WHERE ST_WITHIN(m.geom,j.json_geom)) row) features;
+          """, geoJson.replace("\\\"", "\"").replaceAll("\\s+", ""));
 
-        geoJson = this.cleanJson(geoJson);
+    String retVal = em.createNativeQuery(sql)
+        .getSingleResult()
+        .toString();
 
-        String sql = String.format("""
-                 WITH json_load AS (
-                    SELECT ST_GeomFromGeoJSON('%s'\\:\\:JSONB->'geometry') json_geom
-                )
-                SELECT CAST(jsonb_build_object(
-                    'type',     'FeatureCollection',
-                    'features', jsonb_agg(feature)
-                ) AS TEXT)
-                FROM (
-                  SELECT jsonb_build_object(
-                    'type',       'Feature',
-                    'id',       "MESH_CODE",
-                    'geometry',   ST_AsGeoJSON(geom)\\:\\:JSONB,
-                    'properties', to_jsonb(row) - "MESH_CODE" - 'geom'
-                  ) AS feature
-                  FROM (SELECT "MESH_CODE",
-                    "人口総数",
-                    "０〜１４歳人口総数",
-                    "１５歳以上人口総数",
-                    "１５〜６４歳人口総数",
-                    "２０歳以上人口総数",
-                    "６５歳以上人口総数",
-                    "７５歳以上人口総数",
-                    "外国人人口総数",
-                    "世帯総数",
-                    "一般世帯数",
-                    "１人世帯の一般世帯数",
-                    "２人世帯の一般世帯数",
-                    "３人世帯の一般世帯数",
-                    "４人世帯の一般世帯数",
-                    "５人世帯の一般世帯数",
-                    "６人世帯の一般世帯数",
-                    "７人以上世帯の一般世帯数",
-                    "６歳未満世帯員のいる一般世帯数",
-                    "６５歳以上世帯員のいる一般世帯数",
-                    "世帯主の年齢が２０〜２９歳の１人世帯の一般世帯数",
-                    "高齢単身の一般世帯数",
-                    "高齢夫婦のみの一般世帯数",
-                    geom
-                    FROM mesh4 m,json_load j WHERE ST_OVERLAPS(m.geom,j.json_geom)) row) features;
-                  """, geoJson.replace("\\\"", "\"").replaceAll("\\s+", ""));
+    return retVal;
+  }
 
-        String retVal = em.createNativeQuery(sql)
-                .getSingleResult()
-                .toString();
+  public String queryPoi(String geoJson) {
 
-        return retVal;
-    }
+    geoJson = this.cleanJson(geoJson);
 
-    public String queryPoi(String geoJson) {
+    String sql = String.format("""
+         WITH json_load AS (
+            SELECT ST_GeomFromGeoJSON('%s'\\:\\:JSONB->'geometry') json_geom
+        )
+        SELECT CAST(jsonb_build_object(
+            'type',     'FeatureCollection',
+            'features', jsonb_agg(feature)
+        ) AS TEXT)
+        FROM (
+          SELECT jsonb_build_object(
+            'type',       'Feature',
+            'id',         osm_id,
+            'geometry',   ST_AsGeoJSON(geom)\\:\\:JSONB,
+            'properties', to_jsonb(row) - 'osm_id' - 'geom'
+          ) AS feature
+          FROM (SELECT osm_id,
+            category,
+            name,
+            geom
+            FROM osm_poi m,json_load j WHERE ST_CONTAINS(j.json_geom,m.geom)) row) features;
+          """, geoJson.replace("\\\"", "\"").replaceAll("\\s+", ""));
 
-        geoJson = this.cleanJson(geoJson);
+    String retVal = em.createNativeQuery(sql)
+        .getSingleResult()
+        .toString();
 
-        String sql = String.format("""
-                 WITH json_load AS (
-                    SELECT ST_GeomFromGeoJSON('%s'\\:\\:JSONB->'geometry') json_geom
-                )
-                SELECT CAST(jsonb_build_object(
-                    'type',     'FeatureCollection',
-                    'features', jsonb_agg(feature)
-                ) AS TEXT)
-                FROM (
-                  SELECT jsonb_build_object(
-                    'type',       'Feature',
-                    'id',         osm_id,
-                    'geometry',   ST_AsGeoJSON(geom)\\:\\:JSONB,
-                    'properties', to_jsonb(row) - 'osm_id' - 'geom'
-                  ) AS feature
-                  FROM (SELECT osm_id,
-                    category,
-                    name,
-                    geom
-                    FROM osm_poi m,json_load j WHERE ST_CONTAINS(j.json_geom,m.geom)) row) features;
-                  """, geoJson.replace("\\\"", "\"").replaceAll("\\s+", ""));
+    return retVal;
+  }
 
-        String retVal = em.createNativeQuery(sql)
-                .getSingleResult()
-                .toString();
+  public String queryPeopleFlow(String geoJson, String fieldName) {
+    geoJson = this.cleanJson(geoJson);
 
-        return retVal;
-    }
+    String sql = String.format("""
+         WITH json_load AS (
+            SELECT ST_GeomFromGeoJSON('%s'\\:\\:JSONB->'geometry') geom
+        )
+        SELECT json_object_agg(case when field is null then 'その他' else field end,count)\\:\\:text as t1
+        FROM
+         (SELECT %s as field,count(*) as count
+          FROM
+            (SELECT distinct on (a.userid) c.* FROM geolocation a, json_load b,survey c
+            WHERE st_contains(b.geom,a.geom) and a.userid = c.userid) as q1
+            GROUP BY %s ) as q2  ;
+          """,
+        geoJson.replace("\\\"", "\"").replaceAll("\\s+", ""),
+        fieldName,
+        fieldName);
 
-    public String queryPeopleFlow(String geoJson, String fieldName) {
-        geoJson = this.cleanJson(geoJson);
+    String retVal = em.createNativeQuery(sql)
+        .getSingleResult()
+        .toString();
 
-        String sql = String.format("""
-                 WITH json_load AS (
-                    SELECT ST_GeomFromGeoJSON('%s'\\:\\:JSONB->'geometry') geom
-                )
-                SELECT json_object_agg(case when field is null then 'その他' else field end,count)\\:\\:text as t1
-                FROM
-                 (SELECT %s as field,count(*) as count
-                  FROM
-                    (SELECT distinct on (a.userid) c.* FROM geolocation a, json_load b,survey c
-                    WHERE st_contains(b.geom,a.geom) and a.userid = c.userid) as q1
-                    GROUP BY %s ) as q2  ;
-                  """,
-                geoJson.replace("\\\"", "\"").replaceAll("\\s+", ""),
-                fieldName,
-                fieldName);
-
-        String retVal = em.createNativeQuery(sql)
-                .getSingleResult()
-                .toString();
-
-        return retVal;
-    }
+    return retVal;
+  }
 }
