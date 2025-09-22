8d25ff8ccd832fcadc51cc954f31ff864b8d07af
mbasa <mario.basa@gmail.com>
2025-09-22 11:34:03 +0900

    adding POST request for mesh4 service.

--- src/main/java/org/mbasa/CensusService/controller/ApplicationController.java
+++ src/main/java/org/mbasa/CensusService/controller/ApplicationController.java
@@ -10,6 +10,7 @@ package org.mbasa.CensusService.controller;
 import org.mbasa.CensusService.repository.CustomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,22 +36,25 @@ public class ApplicationController {
     @Autowired
     CustomRepository custRepo;
 
-    @Operation(summary = "Returns 500m mesh census data", 
-            description = "Returns geojson features of 500m mesh data containing Census Information that overlaps input geojson polygon",
-            parameters = {
-                    @Parameter(name = "geoJson", description = "GeoJSON Polygon that will be used to query the mesh census data", required = true)
-                    }
-
-            )
+    @Operation(summary = "Returns 500m mesh census data (GET)", description = "Returns geojson features of 500m mesh data containing Census Information that overlaps input geojson polygon (GET)", parameters = {
+            @Parameter(name = "geoJson", description = "GeoJSON Polygon that will be used to query the mesh census data", required = true)
+    })
     @GetMapping(value = "/census/mesh4", produces = "application/json;charset=UTF-8")
-            public String getMeshData(@RequestParam("geoJson") String geoJson) {
+    public String getMeshDataGet(@RequestParam("geoJson") String geoJson) {
+        return custRepo.queryMesh4(geoJson);
+    }
+
+    @Operation(summary = "Returns 500m mesh census data (POST)", description = "Returns geojson features of 500m mesh data containing Census Information that overlaps input geojson polygon (POST)", parameters = {
+            @Parameter(name = "geoJson", description = "GeoJSON Polygon that will be used to query the mesh census data", required = true)
+    })
+    @PostMapping(value = "/census/mesh4", produces = "application/json;charset=UTF-8")
+    public String getMeshDataPost(@RequestParam("geoJson") String geoJson) {
         return custRepo.queryMesh4(geoJson);
     }
 
     @Operation(summary = "Returns POIs", description = "Returns geojson features of POI data that is contained in the input geojson polygon", parameters = {
             @Parameter(name = "geoJson", description = "GeoJSON Polygon that will be used to query the POIs", required = true)
-    }
-    )
+    })
     @GetMapping(value = "/poi", produces = "application/json;charset=UTF-8")
     public String getPoiData(@RequestParam("geoJson") String geoJson) {
         return custRepo.queryPoi(geoJson);
